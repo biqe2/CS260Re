5fed1d2c2ea6178ecd6b2c771cbf09f02c5df9d4
Badi I. Quinteros <biqe2@byu.edu>
2022-11-03 19:13:45 +0000

    saving

---
+++ Debug/script-photo.js
@@ -0,0 +1,44 @@
+document.getElementById('getButton').addEventListener('click', function(event)
+
+ {
+  event.preventDefault();
+  // get form values
+  let width = document.getElementById('width').value;
+  let height = document.getElementById('height').value;
+  let grayscale = document.getElementById('grayscale').checked;
+  let blur = document.getElementById('blur').checked;
+  let blurAmount = document.getElementById('blurAmount').value;
+  let random = Math.floor(Math.random() * 10000);
+
+  // check if width is empty
+  if (width === "") {
+    width = "200";
+  }
+
+  // setup URL
+  let url = "https://picsum.photos/" + width;
+  if (height !== "") {
+    url += "/" + height;
+  }
+  
+  url += "?random=" + random;
+  
+  if (grayscale) {
+    url += "&grayscale"
+  }
+  if (blur) {
+    url += "&blur=" + blurAmount;
+  }
+
+  updateImage(url);
+}
+
+
+
+);
+
+function updateImage(url) {
+  //debugger
+  document.getElementById('photo').src = url;
+}
+
